55f1e22345808093755e03f24b55b9616acce99a
Craig Peterson <craig.peterson0701@gmail.com>
2023-12-04 20:26:44 -0600

    Update language.py

--- aoc/2023/language.py
+++ aoc/2023/language.py
@@ -1,12 +1,12 @@
 import random
 
-rand_num = random.randint(1, 8)
+rand_num = random.randint(1, 4)
 
-if rand_num == 1 or rand_num == 2:
-    print("Your language is C")
-elif rand_num == 3 or rand_num == 4 or rand_num == 5:
-    print("Your language is C++")
-elif rand_num == 6 or rand_num == 7:
+if rand_num == 1:
     print("Your language is Java")
+elif rand_num == 2:
+    print("Your language is C++")
+elif rand_num == 3:
+    print("Your language is JS")
 else:
     print("Your language is Python")
