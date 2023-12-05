d4f1aa6c269a0470523e0c4b720a483301cb3ee6
Craig Peterson <craig.peterson0701@gmail.com>
2023-12-04 21:04:26 -0600

    Update day1.cpp

--- aoc/2023/day1/day1.cpp
+++ aoc/2023/day1/day1.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include <cstdio>
-#include <cctype>
+#include <unordered_map>
 #include <string>
 
 using namespace std;
 
 int get_digit_forward(string);
 int get_digit_backward(string); 
-int compare_digits(int, string);
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("Usage %s <filename>\n", argv[0]);
+    cout << "Usage " << argv[0] << " <filename>\n";
     return 1;
   }
   
   ifstream file(argv[1]);
   if (!file.is_open()) {
-    printf("Error when opening file\n");
+    cout << "Error when opening file\n";
     return 1; 
   }
 
@@ -29,7 +27,7 @@ int main(int argc, char **argv) {
     int num = 0;
     for (int i = 0; i < line.length(); i++) {
       if (isdigit(line[i])) {
-        int temp = static_cast<int>(line[i]) - static_cast<int>('0');
+        int temp = stoi(string(1, line[i]));
         int letter_temp = get_digit_forward(line.substr(0, i));
         if (letter_temp == -1) {
           num += temp * 10;
@@ -41,7 +39,7 @@ int main(int argc, char **argv) {
     }
     for (int i = line.length() - 1; i >= 0; i--) {
       if (isdigit(line[i])) {
-        int temp = static_cast<int>(line[i]) - static_cast<int>('0');
+        int temp = stoi(string(1, line[i]));
         int letter_temp = get_digit_backward(line.substr(i + 1));
         if (letter_temp == -1) {
           num += temp;
@@ -51,7 +49,7 @@ int main(int argc, char **argv) {
         break;
       }
     }
-    printf("The number for the string at index %d is: %d\n\n", count, num);
+    cout << "The number for the string at index: " << count << " is: " << num << endl;
     sum += num;
     count++;
   }
@@ -62,26 +60,15 @@ int main(int argc, char **argv) {
 }
 
 int get_digit_forward(string substring) {
-  int length = substring.length();
-  for (int i = 0; i < length; i++) {
-    if (i + 2 < substring.length() && substring.compare(i, 3, "one") == 0) {
-      return  1;
-    } else if (i + 2 < substring.length() && substring.compare(i, 3, "two") == 0) {
-      return  2;
-    } else if (i + 4 < substring.length() && substring.compare(i, 5, "three") == 0) {
-      return 3;
-    } else if (i + 3 < substring.length() && substring.compare(i, 4, "four") == 0) {
-      return  4;
-    } else if (i + 3 < substring.length() && substring.compare(i, 4, "five") == 0) {
-      return  5;
-    } else if (i + 2 < substring.length() && substring.compare(i, 3, "six") == 0) {
-      return  6;
-    } else if (i + 4 < substring.length() && substring.compare(i, 5, "seven") == 0) {
-      return  7;
-    } else if (i + 4 < substring.length() && substring.compare(i, 5, "eight") == 0) {
-      return 8;
-    } else if (i + 3 < substring.length() && substring.compare(i, 4, "nine") == 0) {
-      return 9;
+  unordered_map<string, int> word_to_num = {
+    {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+  };
+
+  for (int i = 0; i < substring.length(); i++) {
+    for (const auto &pair : word_to_num) {
+      if (i + pair.first.length() <= substring.length() && substring.compare(i, pair.first.length(), pair.first) == 0) {
+        return pair.second;
+      }
     }
   }
   return -1;
@@ -89,25 +76,15 @@ int get_digit_forward(string substring) {
 
 int get_digit_backward(string substring) {
   int temp_num = -1;
+  unordered_map<string, int> word_to_num = {
+    {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+  };
+
   for (int i = 0; i < substring.length(); i++) {
-    if (i + 2 < substring.length() && substring.compare(i, 3, "one") == 0) {
-      temp_num =  1;
-    } else if (i + 2 < substring.length() && substring.compare(i, 3, "two") == 0) {
-      temp_num =  2;
-    } else if (i + 4 < substring.length() && substring.compare(i, 5, "three") == 0) {
-      temp_num = 3;
-    } else if (i + 3 < substring.length() && substring.compare(i, 4, "four") == 0) {
-      temp_num = 4;
-    } else if (i + 3 < substring.length() && substring.compare(i, 4, "five") == 0) {
-      temp_num = 5;
-    } else if (i + 2 < substring.length() && substring.compare(i, 3, "six") == 0) {
-      temp_num = 6;
-    } else if (i + 4 < substring.length() && substring.compare(i, 5, "seven") == 0) {
-      temp_num = 7;
-    } else if (i + 4 < substring.length() && substring.compare(i, 5, "eight") == 0) {
-      temp_num = 8;
-    } else if (i + 3 < substring.length() && substring.compare(i, 4, "nine") == 0) {
-      temp_num = 9;
+    for (const auto &pair : word_to_num) {
+      if (i + pair.first.length() <= substring.length() && substring.compare(i, pair.first.length(), pair.first) == 0) {
+        temp_num = pair.second;
+      }
     }
   }
   return temp_num;
